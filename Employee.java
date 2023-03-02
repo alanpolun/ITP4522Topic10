0d02d8e1499be08b37b15ddd229e852088eaa3b6
Po Lun Alan <alanpo@alanpo333macmini.local>
2023-03-02 15:21:47 +0800

    The Initial Environment for ITP3914 Lab10 Ex1

---
+++ Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+	private String name;   
+	private int salary;    
+
+	public void setName(String inName) { 
+		name = inName; 
+	}    
+
+	public void setSalary(int inSalary) { 
+		salary = inSalary; 
+	}
+
+	public String getName() { 
+		return name; 
+	}
+
+	public int getSalary() { 
+		return salary; 
+	}
+}
---
+++ Ex1.java
@@ -0,0 +1,20 @@
+public class Ex1 {
+
+  public static void main(String args[]) {
+    // TODO code application logic here
+    Employee emp1 = new Employee();
+    Employee emp2 = new Employee();
+    int oldSalary;
+
+    // Part 1-2 here
+    // Part 3 below
+    System.out.println("Before-");
+    System.out.println("Employee 1: name=" + emp1.getName()
+            + " salary=" + emp1.getSalary());
+    System.out.println("Employee 2: name=" + emp2.getName()
+            + " salary=" + emp2.getSalary());
+
+    // Part 4-5 here
+  }
+}
+
