3ff8772cb95db41df71ff3c889c9219691983b71
Po Lun Alan <alanpo@alanpo333macmini.local>
2023-03-02 15:44:12 +0800

    Use Insert Code to generate getter and setter

--- Employee.java
+++ Employee.java
@@ -2,19 +2,20 @@ public class Employee {
 	private String name;   
 	private int salary;    
 
-	public void setName(String inName) { 
-		name = inName; 
-	}    
+  public String getName() {
+    return name;
+  }
 
-	public void setSalary(int inSalary) { 
-		salary = inSalary; 
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public String getName() { 
-		return name; 
-	}
+  public int getSalary() {
+    return salary;
+  }
 
-	public int getSalary() { 
-		return salary; 
-	}
+  public void setSalary(int salary) {
+    this.salary = salary;
+  }
+
 }
