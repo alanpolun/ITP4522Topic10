d47392620eb86df291374e6a717d3e8e9f8ffcd3
Po Lun Alan <alanpo@alanpo333macmini.local>
2023-03-08 17:00:14 +0800

    Solution for Ex1 (c)

--- Employee.java
+++ Employee.java
@@ -2,6 +2,11 @@ public class Employee {
 	private String name;   
 	private int salary;    
 
+  public Employee(String name, int salary) {
+    this.name = name;
+    this.salary = salary;
+  }
+
 	public void setName(String inName) { 
 		name = inName; 
 	}    
